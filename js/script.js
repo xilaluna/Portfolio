7d7c8765ca4e9b398d05dd0462bdfb8fba11b92f
Xila Luna <xilaluna2@gmail.com>
2021-10-08 01:47:10 -0700

    [fix]carousel resizing

--- js/script.js
+++ js/script.js
@@ -30,3 +30,68 @@
 //     }
 //   )
 // })
+
+function boringstocksCarouselNormalization() {
+  var items = $(".boringstocks-carousel .item"), //grab all slides
+    heights = [], //create empty array to store height values
+    tallest //create variable to make note of the tallest slide
+
+  if (items.length) {
+    function normalizeHeights() {
+      items.each(function () {
+        //add heights to array
+        heights.push($(this).height())
+      })
+      tallest = Math.max.apply(null, heights) //cache largest value
+      items.each(function () {
+        $(this).css("min-height", tallest + "px")
+      })
+    }
+    normalizeHeights()
+
+    $(window).on("resize orientationchange", function () {
+      ;(tallest = 0), (heights.length = 0) //reset vars
+      items.each(function () {
+        $(this).css("min-height", "0") //reset min-height
+      })
+      normalizeHeights() //run it again
+    })
+  }
+}
+
+function popcornCarouselNormalization() {
+  var items = $(".popcorn-carousel .item"), //grab all slides
+    heights = [], //create empty array to store height values
+    tallest //create variable to make note of the tallest slide
+
+  if (items.length) {
+    function normalizeHeights() {
+      items.each(function () {
+        //add heights to array
+        heights.push($(this).height())
+      })
+      tallest = Math.max.apply(null, heights) //cache largest value
+      items.each(function () {
+        $(this).css("min-height", tallest + "px")
+      })
+    }
+    normalizeHeights()
+
+    $(window).on("resize orientationchange", function () {
+      ;(tallest = 0), (heights.length = 0) //reset vars
+      items.each(function () {
+        $(this).css("min-height", "0") //reset min-height
+      })
+      normalizeHeights() //run it again
+    })
+  }
+}
+
+/**
+ * Wait until all the assets have been loaded so a maximum height
+ * can be calculated correctly.
+ */
+window.onload = function () {
+  boringstocksCarouselNormalization()
+  popcornCarouselNormalization()
+}
