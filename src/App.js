2a2b9df347c2015913a9b2778a57492ec612f646
Xila Luna <xilaluna2@gmail.com>
2021-11-23 00:11:26 -0800

    [add] material ui & project comp

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react"
 
 function App() {
   return (
-    <div className="App">
+    <>
       <header className="App-header">
         <h1>Hello World</h1>
       </header>
-    </div>
+    </>
   )
 }
 
