64c44cb23623714df684ad23c3e4c70958cdec25
Xila Luna <xilaluna2@gmail.com>
2021-11-26 19:40:11 -0800

    [update] finish footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,29 @@
 import React from "react"
 import Typography from "@mui/material/Typography"
+import Container from "@mui/material/Container"
+
+function Copyright() {
+  return (
+    <Typography variant="body2" align="center">
+      {"Copyright © "}
+      {new Date().getFullYear()}
+      {" Xila Luna"}
+    </Typography>
+  )
+}
 
 const Footer = () => {
   return (
-    <footer>
-      <Typography>footer</Typography>
-    </footer>
+    <Container
+      component="footer"
+      sx={{
+        borderTop: (theme) => `5px solid black`,
+        mt: 1,
+        py: [5, 5],
+      }}
+    >
+      <Copyright />
+    </Container>
   )
 }
 
