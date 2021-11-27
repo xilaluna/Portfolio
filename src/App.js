7ce85e5febf6b7f7181c0b4c3270a0ca44abeace
Xila Luna <xilaluna2@gmail.com>
2021-11-26 19:39:34 -0800

    [add] section styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from "react"
-import { Container } from "@mui/material"
+import Container from "@mui/material/Container"
 import Header from "./components/Header/Header"
 import Hero from "./components/Hero/Hero"
 import Projects from "./components/Projects/Projects"
 import Skills from "./components/Skills/Skills"
 import Footer from "./components/Footer/Footer"
 
+const sectionStyles = {
+  borderTop: `2px solid black`,
+  borderBottom: `2px solid black`,
+  my: [1, 1],
+  py: [5, 5],
+}
+
 const App = () => {
   return (
     <Container maxWidth="lg">
       <Header />
       <main>
-        <Hero />
-        <Projects />
-        <Skills />
+        <Container sx={sectionStyles}>
+          <Hero />
+        </Container>
+        <Container sx={sectionStyles}>
+          <Projects />
+        </Container>
+        <Container sx={sectionStyles}>
+          <Skills />
+        </Container>
       </main>
       <Footer />
     </Container>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
+
 import CssBaseline from "@mui/material/CssBaseline"
 import { ThemeProvider } from "@mui/material/styles"
 import theme from "./theme/theme"
