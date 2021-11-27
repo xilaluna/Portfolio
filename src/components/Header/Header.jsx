e17e231df32af17140138f20dbe66eef0bdfcaf0
Xila Luna <xilaluna2@gmail.com>
2021-11-26 19:51:44 -0800

    [upd] finish header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,46 @@
 import React from "react"
 import Typography from "@mui/material/Typography"
+import Container from "@mui/material/Container"
+import Grid from "@mui/material/Grid"
+import GitHubIcon from "@mui/icons-material/GitHub"
+import LinkedInIcon from "@mui/icons-material/LinkedIn"
+import TwitterIcon from "@mui/icons-material/Twitter"
+
+const headingStyles = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}
+
+const iconStyles = {
+  mx: [2, 2],
+}
 
 const Header = () => {
   return (
-    <header>
-      <Typography variant="h1" component="h1">
-        Xila Luna
-      </Typography>
-    </header>
+    <Container
+      component="header"
+      disableGutters
+      sx={{
+        borderBottom: (theme) => `5px solid black`,
+        mb: 1,
+        py: [1, 1],
+      }}
+    >
+      <Grid container>
+        <Grid item xs={12} md={3} sx={headingStyles}>
+          <Typography variant="p">👨🏽‍💻 Status: Learning Blockchain</Typography>
+        </Grid>
+        <Grid item xs={12} md={6} sx={headingStyles}>
+          <Typography variant="h1">Xila Luna</Typography>
+        </Grid>
+        <Grid item xs={12} md={3} sx={headingStyles}>
+          <GitHubIcon sx={iconStyles} fontSize="large" />
+          <LinkedInIcon sx={iconStyles} fontSize="large" />
+          <TwitterIcon sx={iconStyles} fontSize="large" />
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
 
