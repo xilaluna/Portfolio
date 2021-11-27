71fa0b1dd54d7b58b14f3345709eedca4c1b2bb0
Xila Luna <xilaluna2@gmail.com>
2021-11-26 22:20:07 -0800

    [upd] finish hero comp

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,64 @@
 import React from "react"
 import Typography from "@mui/material/Typography"
 import Grid from "@mui/material/Grid"
-import Avatar from "@mui/material/Avatar"
+import Box from "@mui/material/Box"
 import profileGif from "../../assets/images/profile-gif.GIF"
+
+const pictureStyles = (theme) => ({
+  [theme.breakpoints.up("xs")]: {
+    width: 300,
+    height: 300,
+  },
+  [theme.breakpoints.up("sm")]: {
+    width: 515,
+    height: 515,
+  },
+  [theme.breakpoints.up("md")]: {
+    width: 300,
+    height: 300,
+  },
+  width: 300,
+  height: "100%",
+  backgroundImage: `url(${profileGif})`,
+  backgroundRepeat: "no-repeat",
+  backgroundColor:
+    theme.palette.mode === "light" ? theme.palette.grey[50] : theme.palette.grey[900],
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  borderRadius: "50%",
+  border: 2,
+})
+
+const statementStyles = (theme) => ({
+  [theme.breakpoints.up("xs")]: {
+    fontSize: 20,
+  },
+
+  [theme.breakpoints.up("sm")]: {
+    fontSize: 30,
+  },
+})
 
 const Hero = () => {
   return (
-    <Grid container>
-      <Grid item xs={12} md={4}>
-        <Avatar alt="Xila Luna" src={profileGif} sx={{ width: 280, height: 280 }} />
+    <Grid container spacing={3}>
+      <Grid
+        item
+        xs={12}
+        sm={12}
+        md={4}
+        sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+      >
+        <Box sx={pictureStyles} />
       </Grid>
-      <Grid item xs={12} md={8}>
-        <Typography variant="h3">
+      <Grid
+        item
+        xs={12}
+        sm={12}
+        md={8}
+        sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+      >
+        <Typography variant="p" component="p" sx={statementStyles}>
           Xila Luna is a creative full-stack engineer who is willing and able to find solutions when
           there are none.
         </Typography>
