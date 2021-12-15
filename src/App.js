81613c24bd61f89280917c6ab7f57531211b411d
Xila Luna <xilaluna2@gmail.com>
2021-12-14 22:03:06 -0800

    [add] paper around

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Paper from "@mui/material/Paper"
 import Container from "@mui/material/Container"
 import Typography from "@mui/material/Typography"
 import Header from "./components/Header/Header"
@@ -21,26 +22,28 @@ const headingStyles = {
 
 const App = () => {
   return (
-    <Container maxWidth="lg">
-      <Header />
-      <main>
-        <Container sx={sectionStyles} disableGutters>
-          <Hero />
-        </Container>
-        <Container sx={sectionStyles} disableGutters>
-          <Typography variant="h4" component="h4" sx={headingStyles}>
-            ✵ PROJECTS ✵
-          </Typography>
-          <Projects />
-        </Container>
-        <Container sx={[sectionStyles, { borderBottom: `2px solid black` }]} disableGutters>
-          <Typography variant="h4" component="h4" sx={headingStyles}>
-            ✵ SKILLS ✵
-          </Typography>
-          <Skills />
-        </Container>
-      </main>
-      <Footer />
+    <Container maxWidth="lg" sx={{ mt: 3, mb: 5 }}>
+      <Paper elevation={15}>
+        <Header />
+        <main>
+          <Container sx={sectionStyles} disableGutters>
+            <Hero />
+          </Container>
+          <Container sx={sectionStyles} disableGutters>
+            <Typography variant="h4" component="h4" sx={headingStyles}>
+              ✵ PROJECTS ✵
+            </Typography>
+            <Projects />
+          </Container>
+          <Container sx={[sectionStyles, { borderBottom: `2px solid black` }]} disableGutters>
+            <Typography variant="h4" component="h4" sx={headingStyles}>
+              ✵ SKILLS ✵
+            </Typography>
+            <Skills />
+          </Container>
+        </main>
+        <Footer />
+      </Paper>
     </Container>
   )
 }
