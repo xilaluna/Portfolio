b10c414a2688f9e529fdf0e4b2737895a0c42a6c
xiluna <xilaluna2@gmail.com>
2022-01-02 03:05:12 -0800

    [rm] bio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,23 +1,8 @@
 import React from "react"
 import Grid from "@mui/material/Grid"
-import Card from "@mui/material/Card"
-import CardContent from "@mui/material/CardContent"
-import Typography from "@mui/material/Typography"
+
 import ListCard from "./ListCard"
 import data from "../../assets/data/about.json"
-
-const cardContentStyles = (theme) => ({
-  px: 0,
-})
-
-const bioBorder = (theme) => ({
-  [theme.breakpoints.up("md")]: {
-    borderRight: "2px solid black",
-  },
-  [theme.breakpoints.down("md")]: {
-    borderBottom: "2px solid black",
-  },
-})
 
 const eduBorder = (theme) => ({
   [theme.breakpoints.only("sm")]: {
@@ -37,12 +22,6 @@ const eduOrder = (theme) => ({
   },
 })
 
-const bioOrder = (theme) => ({
-  [theme.breakpoints.down("md")]: {
-    order: 1,
-  },
-})
-
 const exOrder = (theme) => ({
   [theme.breakpoints.down("md")]: {
     order: 3,
@@ -52,20 +31,11 @@ const exOrder = (theme) => ({
 const About = () => {
   return (
     <Grid container>
-      <Grid item md={3.5} sm={6} xs={12} sx={[eduOrder, eduBorder]}>
+      <Grid item md={6} sm={6} xs={12} sx={[eduOrder, eduBorder]}>
         <ListCard name={data[0].name} list={data[0].list} />
       </Grid>
-      <Grid item md={5} xs={12} sx={[bioBorder, bioOrder]}>
-        <Card elevation={0} square sx={{ px: 2 }}>
-          <CardContent sx={cardContentStyles}>
-            <Typography variant="p" sx={{ fontSize: 19 }}>
-              {data[1].description}
-            </Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item md={3.5} sm={6} xs={12} sx={[exOrder]}>
-        <ListCard name={data[2].name} list={data[2].list} />
+      <Grid item md={6} sm={6} xs={12} sx={[exOrder]}>
+        <ListCard name={data[1].name} list={data[1].list} />
       </Grid>
     </Grid>
   )
