b6078ac10e7dbfff0213c9c853485183b25daaf8
xiluna <xilaluna2@gmail.com>
2022-01-02 03:01:17 -0800

    [add] about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,37 +8,63 @@ import data from "../../assets/data/about.json"
 
 const cardContentStyles = (theme) => ({
   px: 0,
-  py: 0,
 })
 
-const mediumBorderRightStyles = (theme) => ({
-  [theme.breakpoints.up("sm")]: {
+const bioBorder = (theme) => ({
+  [theme.breakpoints.up("md")]: {
     borderRight: "2px solid black",
+  },
+  [theme.breakpoints.down("md")]: {
+    borderBottom: "2px solid black",
   },
 })
 
-const orderStyles = (theme) => ({
-  [theme.breakpoints.up("sm")]: {
+const eduBorder = (theme) => ({
+  [theme.breakpoints.only("sm")]: {
     borderRight: "2px solid black",
+  },
+  [theme.breakpoints.up("md")]: {
+    borderRight: "2px solid black",
+  },
+  [theme.breakpoints.only("xs")]: {
+    borderBottom: "2px solid black",
+  },
+})
+
+const eduOrder = (theme) => ({
+  [theme.breakpoints.down("md")]: {
+    order: 2,
+  },
+})
+
+const bioOrder = (theme) => ({
+  [theme.breakpoints.down("md")]: {
+    order: 1,
+  },
+})
+
+const exOrder = (theme) => ({
+  [theme.breakpoints.down("md")]: {
+    order: 3,
   },
 })
 
 const About = () => {
   return (
     <Grid container>
-      <Grid item md={3.5} sm={4} xs={12} sx={[mediumBorderRightStyles]}>
+      <Grid item md={3.5} sm={6} xs={12} sx={[eduOrder, eduBorder]}>
         <ListCard name={data[0].name} list={data[0].list} />
       </Grid>
-      <Grid item md={5} sm={4} xs={12} sx={[mediumBorderRightStyles]}>
+      <Grid item md={5} xs={12} sx={[bioBorder, bioOrder]}>
         <Card elevation={0} square sx={{ px: 2 }}>
           <CardContent sx={cardContentStyles}>
-            <Typography variant="p" sx={{ fontSize: 18 }}>
+            <Typography variant="p" sx={{ fontSize: 19 }}>
               {data[1].description}
             </Typography>
           </CardContent>
         </Card>
       </Grid>
-      <Grid item md={3.5} sm={4} xs={12}>
+      <Grid item md={3.5} sm={6} xs={12} sx={[exOrder]}>
         <ListCard name={data[2].name} list={data[2].list} />
       </Grid>
     </Grid>
--- src/components/About/ListCard.jsx
+++ src/components/About/ListCard.jsx
@@ -4,28 +4,55 @@ import CardContent from "@mui/material/CardContent"
 import Typography from "@mui/material/Typography"
 import List from "@mui/material/List"
 import ListItem from "@mui/material/ListItem"
-import ListItemText from "@mui/material/ListItemText"
+import Box from "@mui/material/Box"
+
+const titleFont = (theme) => ({
+  textAlign: "center",
+  [theme.breakpoints.down("md")]: {
+    paddingTop: 2,
+  },
+})
 
 const ListCard = (props) => {
   const { name, list } = props
 
   const listItems = list.map((obj) => {
-    const { name, description, date } = obj
+    const { name, location, description, date, image } = obj
     return (
-      <ListItem sx={{ paddingTop: 0 }}>
-        <ListItemText primary={name} secondary={date} />
+      <ListItem
+        sx={{ justifyContent: "flex-start", alignItems: "flex-start", px: 0 }}
+      >
+        <img
+          src={require(`../../assets/images/aboutIcons/${image}`).default}
+          alt={name}
+          height="30px"
+        />
+
+        <Box
+          sx={{
+            paddingTop: 0,
+            paddingLeft: 2,
+            paddingRight: 0,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+          }}
+        >
+          <Typography sx={{ fontSize: 18 }}>{name}</Typography>
+          <Typography variant="body2">{location}</Typography>
+          <Typography color="text.secondary" variant="subtitle2">
+            {date}
+          </Typography>
+          <Typography variant="body2">{description}</Typography>
+        </Box>
       </ListItem>
     )
   })
   return (
     <Card elevation={0} square sx={{ px: 2 }}>
       <CardContent sx={{ px: 0, py: 0 }}>
-        <Typography
-          gutterBottom
-          variant="h5"
-          component="div"
-          sx={{ textAlign: "center" }}
-        >
+        <Typography gutterBottom variant="h5" component="div" sx={titleFont}>
           {name}
         </Typography>
         <List sx={{ py: 0 }}>{listItems}</List>
